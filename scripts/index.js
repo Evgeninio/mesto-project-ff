c20c214dec430f4bd1303bcdfa38d65878aa310e
Евгений Афанасьев <afanizi@mail.ru>
2024-01-21 21:16:51 +0300

    First review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+const content = document.querySelector('.content')
+const places = content.querySelector('.places')
+const placesList = places.querySelector('.places__list')
 
-// @todo: DOM узлы
+function deleteCard(card) {
+	card.remove()
+}
 
-// @todo: Функция создания карточки
+function addCard(cardTitle, imageUrl, deleteCard) {
+	const cardTemplate = document.getElementById('card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+	const deleteButton = cardElement.querySelector('.card__delete-button')
+	cardElement.querySelector('.card__image').src = imageUrl
+	cardElement.querySelector('.card__title').textContent = cardTitle
+	placesList.append(cardElement)
+	cardElement.querySelector('.card__like-button').addEventListener('click', function(evt){
+		evt.target.classList.toggle('card__like-button_is-active')
+	})
+	const removeButtonClick = evt => {
+		const cardItem = evt.target.closest('.card')
+		deleteCard(cardItem)
+	}
+	deleteButton.addEventListener('click', removeButtonClick)
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i ++) {
+	addCard(initialCards[i].name, initialCards[i].link, deleteCard)
+}
